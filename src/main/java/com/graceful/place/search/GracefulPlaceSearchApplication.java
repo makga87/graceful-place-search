050ad80bfdd8cb5a1404a72484cdc800f421db29
Kim HyungGeun <makga87@naver.com>
2024-10-17 15:26:21 +0900

    [CREATE] 장소 검색 서비스 최초 Repository 생성

---
+++ src/main/java/com/graceful/place/search/GracefulPlaceSearchApplication.java
@@ -0,0 +1,13 @@
+package com.graceful.place.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GracefulPlaceSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GracefulPlaceSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graceful/place/search/GracefulPlaceSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graceful.place.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GracefulPlaceSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
