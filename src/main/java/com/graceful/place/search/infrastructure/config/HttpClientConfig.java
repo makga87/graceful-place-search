fccaf06ce4a99834aef5f602822966c6a4d68b1b
Kim HyungGeun <makga87@naver.com>
2024-10-18 15:41:50 +0900

    [FIX] yml 로그 설정 추가 [REMOVE] RestTemplate 로그 인터셉터 제거 및 설정 수정

--- src/main/java/com/graceful/place/search/infrastructure/config/HttpClientConfig.java
+++ src/main/java/com/graceful/place/search/infrastructure/config/HttpClientConfig.java
@@ -1,12 +1,8 @@
 package com.graceful.place.search.infrastructure.config;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-import com.graceful.place.search.infrastructure.interceptor.HttpClientLoggingInterceptor;
 
 @Configuration
 public class HttpClientConfig {
@@ -14,7 +10,6 @@ public class HttpClientConfig {
 	@Bean
 	public RestTemplate restTemplate() {
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.setInterceptors(Collections.singletonList(new HttpClientLoggingInterceptor()));
 		return restTemplate;
 	}
 }
