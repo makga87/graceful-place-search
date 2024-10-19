179243367b4ca1c9c74f605d9a69fab2f12e028e
Kim HyungGeun <makga87@naver.com>
2024-10-20 00:58:56 +0900

    [FEAT] Test code용 설정 파일 생성 [FEAT] 탑 랭크 검색어용 우선순위 큐 빈생성

--- src/main/java/com/graceful/place/search/infrastructure/config/CacheConfig.java
+++ src/main/java/com/graceful/place/search/infrastructure/config/CacheConfig.java
@@ -1,15 +1,20 @@
 package com.graceful.place.search.infrastructure.config;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 import javax.cache.CacheManager;
 import javax.cache.Caching;
 import javax.cache.spi.CachingProvider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+
+import com.graceful.place.search.domain.Keyword;
 
 @Configuration
 public class CacheConfig {
@@ -19,11 +24,17 @@ public class CacheConfig {
 		return new ConcurrentSkipListMap<>();
 	}
 
+	@Bean
+	public PriorityQueue<Keyword> rankedKeywordsQueue() {
+		return new PriorityQueue<>();
+	}
+
 	@Bean(name = "ehCacheManager")
-	public CacheManager ehCacheManager() throws Exception {
+	public CacheManager defaultEhCacheManager(@Value("${spring.cache.jcache.config}") String configPath) throws Exception {
 		CachingProvider provider = Caching.getCachingProvider();
-		ClassPathResource cacheConfigResource = new ClassPathResource("/ehcache.xml");
+		ClassPathResource cacheConfigResource = new ClassPathResource(configPath);
 		File cacheConfigFile = cacheConfigResource.getFile();
+
 		return provider.getCacheManager(cacheConfigFile.toURI(), getClass().getClassLoader());
 	}
 
