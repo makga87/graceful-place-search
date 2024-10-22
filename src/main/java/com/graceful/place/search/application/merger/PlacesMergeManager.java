05773f089aa7c1d866a96b2e09a1447e7c588b26
Kim HyungGeun <makga87@naver.com>
2024-10-22 11:04:38 +0900

    [REMOVE] 불필요 임포트 제거

--- src/main/java/com/graceful/place/search/application/merger/PlacesMergeManager.java
+++ src/main/java/com/graceful/place/search/application/merger/PlacesMergeManager.java
@@ -1,7 +1,6 @@
 package com.graceful.place.search.application.merger;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
