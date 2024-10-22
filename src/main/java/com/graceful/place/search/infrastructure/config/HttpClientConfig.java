38ca86012703a3824f98bb302dff04e1e22b980d
Kim HyungGeun <makga87@naver.com>
2024-10-23 01:10:17 +0900

    [FEAT] 커넥션 풀 추가

--- src/main/java/com/graceful/place/search/infrastructure/config/HttpClientConfig.java
+++ src/main/java/com/graceful/place/search/infrastructure/config/HttpClientConfig.java
@@ -1,5 +1,6 @@
 package com.graceful.place.search.infrastructure.config;
 
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,14 @@ public class HttpClientConfig {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
 
-
 		factory.setConnectTimeout(2000);
 		factory.setReadTimeout(3000);
 		factory.setConnectionRequestTimeout(3000);
 		factory.setBufferRequestBody(false);
-
+		factory.setHttpClient(HttpClientBuilder.create()
+											   .setMaxConnTotal(50)
+											   .setMaxConnPerRoute(25)
+											   .build());
 		return builder.requestFactory(() -> factory)
 					  .build();
 	}
